253072d7ee68e23f59fa29e18a71f8b8011dd13b
KaMaZ <kamaz@xsimple.org>
2016-11-11 21:44:38 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cp .vimrc $HOME
+cp .tmux.conf $HOME
+
