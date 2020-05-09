b539e0fd685741b5b395740b43a42f1f10c46094
Kamaz <kamaz@xsimple.org>
2020-05-09 21:12:06 +0100

    add install .config directory

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+cp -r .config $HOME
 cp .ideavimrc $HOME
 cp .gitconfig $HOME
 cp .vimrc $HOME
